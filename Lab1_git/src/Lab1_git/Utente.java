6a5a2574e89922c3366cf934243928fb51c2c188
Beatriz Antunes <biasoaresantunes15@gmail.com>
2025-09-12 12:42:02 +0100

    1ºversão da classe Utente

---
+++ Lab1_git/src/Lab1_git/Utente.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package Lab1_git;
+
+/**
+ * @author 51301
+ */
+
+public class Utente {
+	private String nome;
+	private String genero;
+	private int idade;
+	private int peso;
+	private float altura;
+	
+	/**
+	 * @param nome
+	 * @param genero
+	 * @param idade
+	 * @param peso
+	 * @param altura
+	 */
+	
+	public Utente(String nome, String genero, int idade, int peso, float altura) {
+		super();
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	
+	
+	}
+}
